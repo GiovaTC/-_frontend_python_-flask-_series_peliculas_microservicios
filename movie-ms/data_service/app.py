5b0664f011a58ed0b767b3cfd4621a368eadf56c
gtapiero <giovanny.tapiero@pi.edu.co>
2025-09-17 17:28:16 -0500

    commit inicial

---
+++ movie-ms/data_service/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+import os, time, requests
+from dotenv import load_dotenv
+from threading import Lock
+
+load_dotenv()
+TMDB_KEY = os.getenv("TMDB_API_KEY")
+app = Flask(__name__)
+
+#cache simple en memoria con TTL
+_cache, _cache_lock = {}, Lock()
+CACHE_TTL =  60 * 5 # 5 minutos
+
+def set_cache(key, value):
+    with _cache_lock:
+        _cache[key] = (value, time.time())
+
+def get_cache(key):
+    with _cache_lock:
+        item = _cache.get(key)
+        if not item: return None
+        value, ts = item
+        if time.time() - ts > CACHE_TTL:
+            del_cache[key]
+            return None
+        return value
